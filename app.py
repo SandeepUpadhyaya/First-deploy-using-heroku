5f6e0fa6c7fd5387aa0cc1048377cb21a0967f72
Sandeep K <79915877+SandeepUpadhyaya@users.noreply.github.com>
2022-02-08 13:28:55 +0530

    app and index updated

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
+
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/v1/predict',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
@@ -22,6 +23,17 @@ def predict():
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
+@app.route('/v2/predict',methods=['POST'])
+def predictRupees1():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = (model.predict(final_features)) * 70
 
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be Rupees {}'.format(output))
 if __name__ == "__main__":
     app.run(debug=True)
